7c06339f28c915a30895d4395e65a45fff3c449b
ohsgottherizz <ohchangmin2007@gmail.com>
2024-01-31 11:24:15 +0900

    DI

---
+++ backend/books/allinone/calculator2/src/MyCalculator.java
@@ -0,0 +1,6 @@
+public class MyCalculator {
+    public void calculate(int fNum, int sNum, ICalculator calculator) {
+        int value = calculator.doOperation(fNum, sNum);
+        System.out.println("result : " + value);
+    }
+}

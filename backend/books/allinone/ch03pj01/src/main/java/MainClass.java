59e8d4bcc0500ebf646de78d7775ed408a9a9bba
ohsgottherizz <ohchangmin2007@gmail.com>
2024-01-31 12:07:32 +0900

    I love beans

---
+++ backend/books/allinone/ch03pj01/src/main/java/MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass {
+    public static void main(String[] args) {
+        TransportationWalk transportationWalk = new TransportationWalk();
+
+        transportationWalk.move();
+    }
+}
---
+++ backend/books/allinone/ch03pj01/src/main/java/TransportationWalk.java
@@ -0,0 +1,5 @@
+public class TransportationWalk {
+    public void move() {
+        System.out.println("도보로 이동합니다!");
+    }
+}

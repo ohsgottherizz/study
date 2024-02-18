d6547bdd97b579ae7bb0d8cf1197ee7fca7c2f79
ohsgottherizz <ohchangmin2007@gmail.com>
2024-02-18 16:07:23 +0900

    Using BEANSgit add .

---
+++ backend/books/allinone/ch03_pjt_01/src/main/java/spring5/MainClass.java
@@ -0,0 +1,8 @@
+package spring5;
+
+public class MainClass {
+    public static void main(String[] args) {
+        TransportationWalk transportationWalk = new TransportationWalk();
+        transportationWalk.move();
+    }
+}ch03_pjt_01

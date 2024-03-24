42ceb5d21181ca472bcb22be77065100d7ef96f1
ohsgottherizz <ohchangmin2007@gmail.com>
2024-03-24 10:45:27 +0900

    I'm back

---
+++ backend/books/allinone/ch04_pjt_01/src/main/java/ems/member/service/StudentAllSelectService.java
@@ -0,0 +1,20 @@
+package ems.member.service;
+
+import ch04_pjt_01.ems.member.Student;
+import ch04_pjt_01.ems.member.dao.StudentDao;
+
+import java.util.Map;
+
+public class StudentAllSelectService {
+
+	private StudentDao studentDao;
+
+	public StudentAllSelectService(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+
+	public Map<String, Student> allSelect() {
+		return studentDao.getStudentDB();
+	}
+
+}
---
+++ backend/books/allinone/ch04_pjt_01/src/test/java/org/example/ch04_pjt_01/Ch04Pjt01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ch04_pjt_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04Pjt01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

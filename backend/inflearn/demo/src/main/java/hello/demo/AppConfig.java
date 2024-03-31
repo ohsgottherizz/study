f4840d38925baea132e4fc405c0bc60b05492bb1
ohsgottherizz <ohchangmin2007@gmail.com>
2024-03-31 14:40:54 +0900

    ocp

--- backend/inflearn/demo/src/main/java/hello/demo/AppConfig.java
+++ backend/inflearn/demo/src/main/java/hello/demo/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.demo;
 
+import hello.demo.discount.DiscountPolicy;
 import hello.demo.discount.FixDiscountPolicy;
+import hello.demo.discount.RateDiscountPolicy;
 import hello.demo.member.MemberService;
 import hello.demo.member.MemberServiceImpl;
 import hello.demo.member.MemoryMemberRespository;
@@ -9,10 +11,18 @@ import hello.demo.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRespository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRespository memberRepository() {
+        return new MemoryMemberRespository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRespository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }
--- backend/inflearn/demo/src/main/java/hello/demo/order/OrderService.java
+++ backend/inflearn/demo/src/main/java/hello/demo/order/OrderService.java
@@ -2,6 +2,4 @@ package hello.demo.order;
 
 public interface OrderService {
     Order createOrder(Long memberId, String itemName, int itemPrice);
-
-
 }
